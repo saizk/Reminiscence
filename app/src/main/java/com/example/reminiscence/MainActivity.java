24a415c72e58a019abfea2bcefb6be7a6b68a5b7
saizk <sergioaizcorbe@hotmail.com>
2021-03-28 00:24:08 +0100

    Added bottom mapping

--- app/src/main/java/com/example/reminiscence/MainActivity.java
+++ app/src/main/java/com/example/reminiscence/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.reminiscence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onAlarmClick(View view){
+        Intent intent = new Intent(this, AlarmActivity.class);
+        startActivity(intent);
+    }
+    public void onResourcesClick(View view){
+        Intent intent = new Intent(this, ResourcesActivity.class);
+        startActivity(intent);
+    }
+    public void onMapClick(View view){
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+    public void onNotepadClick(View view){
+        Intent intent = new Intent(this, NotepadActivity.class);
+        startActivity(intent);
+    }
+
 }
