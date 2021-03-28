8a9d6abd696ae4419c1444f4189daa9cb6d52306
bernibzs <bernardo.bouzas.garcia@gmail.com>
2021-03-28 01:26:48 +0100

    resources button shows a list with alzh organizations

--- app/src/main/java/com/example/reminiscence/ResourcesActivity.java
+++ app/src/main/java/com/example/reminiscence/ResourcesActivity.java
@@ -3,12 +3,73 @@ package com.example.reminiscence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ResourcesActivity extends AppCompatActivity {
+
+
+    ListView list;
+
+    String[] maintitle ={
+            "Confederacion Española de Alzheimer","Asociacion Española de Alzheimer",
+            "Fundacion Pasqual Maragall","FAFAL",
+            "Alzheimer’s Association ESPAÑA",
+    };
+
+    String[] subtitle ={
+            "www.ceafa.es","www.alzfae.es",
+            "www.fpmaragall.org","www.fafal.org",
+            "www.alz.org/es",
+    };
+
+    Integer[] imgid={
+            R.drawable.logo_ceafa,R.drawable.logo_alzfae,
+            R.drawable.logo_fpmaragall,R.drawable.logo_fafal,
+            R.drawable.logo_alz,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
+
+        MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO Auto-generated method stub
+                if(position == 0) {
+                    //code specific to first list item
+                    Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 1) {
+                    //code specific to 2nd list item
+                    Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 2) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 3) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 4) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
