b912274bea76a2807e75be7b7c6f27554e5209cd
bernibzs <bernardo.bouzas.garcia@gmail.com>
2021-05-09 19:20:10 +0200

    alarm interface

--- app/src/main/java/com/example/reminiscence/AlarmActivity.java
+++ app/src/main/java/com/example/reminiscence/AlarmActivity.java
@@ -9,13 +9,14 @@ import android.provider.AlarmClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class AlarmActivity extends AppCompatActivity {
 
     EditText mHourEditText;
     EditText mMinuteEditText;
 
-    Button mSetAlarmButton;
+    ImageButton mSetAlarmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +27,7 @@ public class AlarmActivity extends AppCompatActivity {
         mHourEditText = (EditText) findViewById(R.id.set_alarm_hour);
         mMinuteEditText = (EditText) findViewById(R.id.set_alarm_minute);
 
-        mSetAlarmButton = (Button) findViewById(R.id.set_alarm_button);
+        mSetAlarmButton = (ImageButton) findViewById(R.id.set_alarm_button);
         mSetAlarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
