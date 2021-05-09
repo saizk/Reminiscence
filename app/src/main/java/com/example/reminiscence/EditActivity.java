3a3de117ec453026cb5d65267191b8b962045f27
saizk <sergioaizcorbe@hotmail.com>
2021-05-09 17:49:51 +0200

    Added non functional notepad :P

---
+++ app/src/main/java/com/example/reminiscence/EditActivity.java
@@ -0,0 +1,100 @@
+package com.example.reminiscence;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditActivity extends AppCompatActivity {
+
+    private EditText mTitleText;
+    private EditText mBodyText;
+    private Long mRowId;
+    private NotesDbAdapter dbAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // infla el layout
+        setContentView(R.layout.activity_edit);
+
+        // obtiene referencia a los tres views que componen el layout
+        mTitleText = (EditText) findViewById(R.id.title);
+        mBodyText = (EditText) findViewById(R.id.body);
+        Button confirmButton = (Button) findViewById(R.id.confirm);
+
+        //creamos el adaptador de la BD y la abrimos
+        dbAdapter = new NotesDbAdapter(this);
+        dbAdapter.open();
+
+        // obtiene id de fila de la tabla si se le ha pasado (hemos pulsado una nota para editarla)
+        mRowId = (savedInstanceState == null) ? null :
+                (Long) savedInstanceState.getSerializable(NotesDbAdapter.KEY_ROWID);
+        if (mRowId == null) {
+            Bundle extras = getIntent().getExtras();
+            mRowId = extras != null ? extras.getLong(NotesDbAdapter.KEY_ROWID) : null;
+        }
+
+        // Si se le ha pasado un id (no era null) rellena el título y el cuerpo con los campos guardados en la BD
+        // en caso contrario se dejan en blanco (editamos una nota nueva)
+        if (mRowId != null) {
+            Cursor note = dbAdapter.fetchNote(mRowId);
+            mTitleText.setText(note.getString(
+                    note.getColumnIndexOrThrow(NotesDbAdapter.KEY_TITLE)));
+            mBodyText.setText(note.getString(
+                    note.getColumnIndexOrThrow(NotesDbAdapter.KEY_BODY)));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Se recrea el menu que aparece en ActionBar de la actividad.
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Gestiona la seleccion de opciones en el menú
+        int id = item.getItemId();
+        if (id == R.id.action_delete) {
+            if (mRowId != null) {
+                dbAdapter.deleteNote(mRowId);
+            }
+            setResult(RESULT_OK);
+            dbAdapter.close();
+            finish();
+        }
+
+        if (id == R.id.action_about) {
+            System.out.println("APPMOV: About action...");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void saveNote(View view) {
+        String title = mTitleText.getText().toString();
+        String body = mBodyText.getText().toString();
+
+        if (mRowId == null) {
+            long id = dbAdapter.createNote(title, body);
+            if (id > 0) {
+                mRowId = id;
+            }
+        } else {
+            dbAdapter.updateNote(mRowId, title, body);
+        }
+        setResult(RESULT_OK);
+        dbAdapter.close();
+        finish();
+    }
+
+}
--- app/src/main/java/com/example/reminiscence/NotepadActivity.java
+++ app/src/main/java/com/example/reminiscence/NotepadActivity.java
@@ -5,69 +5,102 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import android.content.Intent;
+import android.database.Cursor;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
 
 public class NotepadActivity extends AppCompatActivity {
-    EditText CreateNote;
+    private NotesDbAdapter dbAdapter;
+    private ListView m_listview;
+
+    // para indicar en un Intent si se quiere crear una nueva nota o editar una existente
+    private static final int ACTIVITY_CREATE=0;
+    private static final int ACTIVITY_EDIT=1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notepad);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Save("Note.txt");
-            }
-        });
+        dbAdapter = new NotesDbAdapter(this);
+        dbAdapter.open();
 
-        CreateNote = (EditText) findViewById(R.id.CreateNote);
-    }
-    public void Save(String fileName) {
-        try {
-            OutputStreamWriter out =
-                    new OutputStreamWriter(openFileOutput(fileName, 0));
-            out.write(String.valueOf(CreateNote));
-            out.close();
-            Toast.makeText(this, "Note Saved!", Toast.LENGTH_SHORT).show();
-        } catch (Throwable t) {
-            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
-        }
-    }
-    public boolean FileExists(String fname){
-        File file = getBaseContext().getFileStreamPath(fname);
-        return file.exists();
-    }
-    public String Open(String fileName) {
-        String content = "";
-        if (FileExists(fileName)) {
-            try {
-                InputStream in = openFileInput(fileName);
-                if ( in != null) {
-                    InputStreamReader tmp = new InputStreamReader( in );
-                    BufferedReader reader = new BufferedReader(tmp);
-                    String str;
-                    StringBuilder buf = new StringBuilder();
-                    while ((str = reader.readLine()) != null) {
-                        buf.append(str).append("\n");
-                    } in .close();
-                    content = buf.toString();
+        // Creamos un listview que va a contener el título de todas las notas y
+        // en el que cuando pulsemos sobre un título lancemos una actividad de editar
+        // la nota con el id correspondiente
+        m_listview = (ListView) findViewById(R.id.id_list_view);
+        m_listview.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
+                        Intent i = new Intent(view.getContext(), EditActivity.class);
+                        i.putExtra(NotesDbAdapter.KEY_ROWID, id);
+                        startActivityForResult(i, ACTIVITY_EDIT);
+                    }
                 }
-            } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
-                Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
-            }
+        );
+
+        // rellenamos el listview con los títulos de todas las notas en la BD
+        fillData();
+    }
+
+    private void fillData() {
+        Cursor notesCursor = dbAdapter.fetchAllNotes();
+
+        // Creamos un array con los campos que queremos mostrar en el listview (sólo el título de la nota)
+        String[] from = new String[]{NotesDbAdapter.KEY_TITLE};
+
+        // array con los campos que queremos ligar a los campos del array de la línea anterior (en este caso sólo text1)
+        int[] to = new int[]{R.id.text1};
+
+        // Creamos un SimpleCursorAdapter y lo asignamos al listview para mostrarlo
+        SimpleCursorAdapter notes =
+                new SimpleCursorAdapter(this, R.layout.notes_row, notesCursor, from, to, 0);
+        m_listview.setAdapter(notes);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Se recrea el menu que aparece en ActionBar de la actividad.
+        getMenuInflater().inflate(R.menu.menu_notepad, menu);
+        return true;
+    }
+
+    // con AppCompatActivity hay que sobreescribir onOptionsItemSelected en lugar de
+    // onMenuItemSelected para gestionar el menú
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Gestiona la seleccion de opciones en el menú
+        int id = item.getItemId();
+        if (id == R.id.action_insert) {
+            createNote();
+            return true;
         }
-        return content;
+
+        if (id == R.id.action_about) {
+            System.out.println("APPMOV: About action...");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createNote() {
+        Intent i = new Intent(this, EditActivity.class);
+        startActivityForResult(i, ACTIVITY_CREATE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        fillData();
     }
 }
