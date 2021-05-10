ae5ee313e98e3ca13deda8ceb7cfe010fdebb789
saizk <sergioaizcorbe@hotmail.com>
2021-05-10 19:31:36 +0200

    Fixed maps

--- app/src/main/java/com/example/reminiscence/MapActivity.java
+++ app/src/main/java/com/example/reminiscence/MapActivity.java
@@ -1,46 +1,147 @@
 package com.example.reminiscence;
 
 import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 
+
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private static final String TAG = MapActivity.class.getSimpleName();
+
+    private static final String SELECTED_STYLE = "selected_style";
+
+    // Stores the ID of the currently selected style, so that we can re-apply it when
+    // the activity restores state, for example when the device changes orientation.
+    private int mSelectedStyleId = R.string.style_label_default;
+
+    // These are simply the string resource IDs for each of the style names. We use them
+    // as identifiers when choosing which style to apply.
+    private int mStyleIds[] = {
+            R.string.style_label_retro,
+            R.string.style_label_night,
+            R.string.style_label_default,
+    };
+
+    private static final LatLng SYDNEY = new LatLng(-33.8688, 151.2093);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            mSelectedStyleId = savedInstanceState.getInt(SELECTED_STYLE);
+        }
         setContentView(R.layout.activity_map);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng leganes = new LatLng(40.33771197613952, -3.772277413917975);
-        mMap.addMarker(new MarkerOptions().position(leganes).title("jambos here!"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(leganes));
+    public void onSaveInstanceState(Bundle outState) {
+        // Store the selected map style, so we can assign it when the activity resumes.
+        outState.putInt(SELECTED_STYLE, mSelectedStyleId);
+        super.onSaveInstanceState(outState);
     }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, 14));
+        setSelectedStyle();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.styled_map_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_style_choose) {
+            showStylesDialog();
+        }
+        return true;
+    }
+
+    /**
+     * Shows a dialog listing the styles to choose from, and applies the selected
+     * style when chosen.
+     */
+    private void showStylesDialog() {
+        // mStyleIds stores each style's resource ID, and we extract the names here, rather
+        // than using an XML array resource which AlertDialog.Builder.setItems() can also
+        // accept. We do this since using an array resource would mean we would not have
+        // constant values we can switch/case on, when choosing which style to apply.
+        List<String> styleNames = new ArrayList<>();
+        for (int style : mStyleIds) {
+            styleNames.add(getString(style));
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.style_choose));
+        builder.setItems(styleNames.toArray(new CharSequence[styleNames.size()]),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mSelectedStyleId = mStyleIds[which];
+                        String msg = getString(R.string.style_set_to, getString(mSelectedStyleId));
+                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, msg);
+                        setSelectedStyle();
+                    }
+                });
+        builder.show();
+    }
+
+    /**
+     * Creates a {@link MapStyleOptions} object via loadRawResourceStyle() (or via the
+     * constructor with a JSON String), then sets it on the {@link GoogleMap} instance,
+     * via the setMapStyle() method.
+     */
+    private void setSelectedStyle() {
+        MapStyleOptions style;
+        switch (mSelectedStyleId) {
+            case R.string.style_label_retro:
+                // Sets the retro style via raw resource JSON.
+                style = MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle_retro);
+                break;
+            case R.string.style_label_night:
+                // Sets the night style via raw resource JSON.
+                style = MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle_night);
+                break;
+            case R.string.style_label_default:
+                // Removes previously set style, by setting it to null.
+                style = null;
+                break;
+            default:
+                return;
+        }
+        mMap.setMapStyle(style);
+    }
+
 }
