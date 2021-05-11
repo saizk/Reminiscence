7a9263b37e1b3c44b17cfc85d6e9e10b4e9d3d53
bernibzs <bernardo.bouzas.garcia@gmail.com>
2021-05-11 22:32:25 +0200

    complete gallery interfaces

--- app/src/main/java/com/example/reminiscence/AddPhotoViewActivity.java
+++ app/src/main/java/com/example/reminiscence/AddPhotoViewActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -24,9 +25,9 @@ public class AddPhotoViewActivity extends AppCompatActivity {
 
 
     ImageView imageView;
-    Button btnPick;
+    ImageButton btnPick;
     EditText inputName;
-    Button saveImageButton;
+    ImageButton saveImageButton;
 
 
 
